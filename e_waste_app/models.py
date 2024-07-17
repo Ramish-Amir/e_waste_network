dffd89dd8694793b334aaadeeed2904a21f31fbb
Naman Goyal <raghavgoyal151101@gmail.com>
2024-07-17 18:24:55 -0400

    Added Home Page, About Page, Contact Us Page And Footer

--- e_waste_app/models.py
+++ e_waste_app/models.py
@@ -1,23 +1,19 @@
 from django.db import models
-# Create your models here.
-from django.db import models
-from django.contrib.auth.models import User
 
-
-class Member(User):
-    USER_TYPE_CHOICES = [
-        ('individual', 'Individual'),
-        ('organization', 'Organization'),
-    ]
-    phone_number = models.CharField(max_length=15, blank=True, null=True)
-    address = models.CharField(max_length=300, blank=True, null=True)
-    city = models.CharField(max_length=50)
-    province = models.CharField(max_length=50)
-    postal_code = models.CharField(max_length=20, blank=True, null=True)
-    country = models.CharField(max_length=50, blank=True, null=True)
-    user_type = models.CharField(max_length=20, choices=USER_TYPE_CHOICES)
-    e_waste_interests = models.TextField(blank=True, null=True)
-    recycling_preferences = models.TextField(blank=True, null=True)
+class ContactMessage(models.Model):
+    name = models.CharField(max_length=100)
+    email = models.EmailField()
+    message = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
-        return self.username
+        return self.name
+
+class Product(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.TextField()
+    address = models.CharField(max_length=200)
+    zipcode = models.CharField(max_length=10)
+
+    def __str__(self):
+        return self.name
--- e_waste_app/views.py
+++ e_waste_app/views.py
@@ -1,8 +1,51 @@
-from django.http import HttpResponse
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib import messages
+from .forms import ContactForm
+from .models import Product  # Assuming Product is your model
 
 # Create your views here.
+def home(request):
+    return render(request,'e_waste_app/home.html')
+def footer(request):
+    return render(request,'e_waste_app/footer.html')
+
+def aboutus(request):
+    return render(request,'e_waste_app/aboutus.html')
+
+def article1(request):
+    return render(request, 'e_waste_app/article1.html')
+
+def article2(request):
+    return render(request, 'e_waste_app/article2.html')
+
+def article3(request):
+    return render(request, 'e_waste_app/article3.html')
+
+def contact_us(request):
+    if request.method == 'POST':
+        form = ContactForm(request.POST)
+        if form.is_valid():
+            form.save()
+            messages.success(request, 'Your message has been sent successfully!')
+            return redirect('contactus')
+    else:
+        form = ContactForm()
+    return render(request, 'e_waste_app/contact_us.html', {'form': form})
 
 
-def index(request):
-    return HttpResponse("Hello, world. You're at the polls index.")
+def home(request):
+    return render(request, 'e_waste_app/home.html')
+
+def search_results(request):
+    name_query = request.GET.get('name', '')
+    zipcode_query = request.GET.get('zipcode', '')
+
+    # Filter products based on name and zipcode
+    products = Product.objects.filter(name__icontains=name_query, zipcode=zipcode_query)
+
+    context = {
+        'products': products
+    }
+    return render(request, 'e_waste_app/search_results.html', context)
+
+
