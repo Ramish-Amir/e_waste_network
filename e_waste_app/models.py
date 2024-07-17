b96afeeeeed9295707a6eaaa757e3a635da6c073
Ramish Amir <ramishamir2750@gmail.com>
2024-07-17 18:49:57 -0400

    Added removed member model

--- e_waste_app/models.py
+++ e_waste_app/models.py
@@ -1,4 +1,25 @@
+from django.contrib.auth.models import User
 from django.db import models
+
+
+class Member(User):
+    USER_TYPE_CHOICES = [
+        ('individual', 'Individual'),
+        ('organization', 'Organization'),
+    ]
+    phone_number = models.CharField(max_length=15, blank=True, null=True)
+    address = models.CharField(max_length=300, blank=True, null=True)
+    city = models.CharField(max_length=50)
+    province = models.CharField(max_length=50)
+    postal_code = models.CharField(max_length=20, blank=True, null=True)
+    country = models.CharField(max_length=50, blank=True, null=True)
+    user_type = models.CharField(max_length=20, choices=USER_TYPE_CHOICES)
+    e_waste_interests = models.TextField(blank=True, null=True)
+    recycling_preferences = models.TextField(blank=True, null=True)
+
+    def __str__(self):
+        return self.username
+
 
 class ContactMessage(models.Model):
     name = models.CharField(max_length=100)
@@ -8,6 +29,7 @@ class ContactMessage(models.Model):
 
     def __str__(self):
         return self.name
+
 
 class Product(models.Model):
     name = models.CharField(max_length=100)
