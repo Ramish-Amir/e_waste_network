5c57c1fc1d83d7860d41bc8eca26650831230713
amit097c <amit097@yahoo.com>
2024-07-17 20:19:45 -0400

    login and register feature done

---
+++ e_waste_app/forms.py
@@ -0,0 +1,38 @@
+from django import forms
+from django.contrib.auth.models import User
+
+
+class LoginForm(forms.Form):
+    username = forms.CharField(widget=forms.TextInput(attrs={'class': "form-control",
+                                                             'id': 'inputUsername3',
+                                                             'placeholder': 'username'}))
+    password = forms.CharField(widget=forms.PasswordInput({'class': 'form-control',
+                                                           'id': 'inputPassword3',
+                                                           'placeholder': 'password'}))
+
+
+class RegisterForm(forms.Form):
+    username = forms.CharField(widget=forms.TextInput(attrs={'class': 'form-control'}))
+    email = forms.EmailField()
+    password = forms.CharField(widget=forms.PasswordInput(attrs={'class': 'form-control'}))
+    confirm_password = forms.CharField(widget=forms.PasswordInput(attrs={'class': 'form-control'}))
+
+    def clean(self):
+        cleaned_data = super(RegisterForm, self).clean()
+        password = cleaned_data.get('password')
+        confirm_password = cleaned_data.get('confirm_password')
+        if password and confirm_password and password != confirm_password:
+            raise forms.ValidationError('Passwords must match')
+        return cleaned_data
+
+    def save(self):
+        username = self.cleaned_data.get('username')
+        email = self.cleaned_data.get('email')
+        password = self.cleaned_data.get('password')
+
+        user = User.objects.create_user(username=username, email=email, password=password)
+        return user
+
+
+class PasswordResetForm(forms.Form):
+    email = forms.EmailField()
